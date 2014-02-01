5486bc96e2ec61600d9d7fb17266a23e3b6d6d00
Ivan Muller <mullerivan@gmail.com>
2014-02-02 11:47:38 +1300

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+rm -fr /home/ivan/.bashrc ;ln -s /home/ivan/projectos/dotfiles/.bashrc  /home/ivan/.bashrc
+rm -fr /home/ivan/.gitconfig ;ln -s /home/ivan/projectos/dotfiles/.gitconfig  /home/ivan/.gitconfig
+rm -fr /home/ivan/.vimrc ;ln -s /home/ivan/projectos/dotfiles/.vimrc  /home/ivan/.vimrc
+rm -fr /home/ivan/.vim ;ln -s /home/ivan/projectos/dotfiles/.vim  /home/ivan/.vim
+rm -fr  /home/ivan/.gemrc; ln -s /home/ivan/projectos/dotfiles/.gemrc  /home/ivan/.gemrc 
+
+
