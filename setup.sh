98e83850ffc019f00e6155745e02b0ce66625088
Ivan Muller <mullerivan@gmail.com>
2015-05-20 10:40:23 +1200

    dotfiles

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ rm -fr /home/ivan/.bashrc ;ln -s /home/ivan/projectos/dotfiles/.bashrc  /home/iv
 rm -fr /home/ivan/.gitconfig ;ln -s /home/ivan/projectos/dotfiles/.gitconfig  /home/ivan/.gitconfig
 rm -fr /home/ivan/.vimrc ;ln -s /home/ivan/projectos/dotfiles/.vimrc  /home/ivan/.vimrc
 rm -fr /home/ivan/.vim ;ln -s /home/ivan/projectos/dotfiles/.vim  /home/ivan/.vim
+rm -fr /home/ivan/.hgrc ;ln -s /home/ivan/projectos/dotfiles/.hgrc  /home/ivan/.hgrc
 rm -fr  /home/ivan/.gemrc; ln -s /home/ivan/projectos/dotfiles/.gemrc  /home/ivan/.gemrc
 rm -fr  /etc/rc.local; ln -s /home/ivan/projectos/dotfiles/rc.local  /etc/rc.local
 #hosts files
