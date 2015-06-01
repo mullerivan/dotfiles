c27105e58861914a49b4763a753820a2e954c686
Ivan Muller <mullerivan@gmail.com>
2015-06-02 01:52:42 +0200

    zshrc

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 rm -fr /home/ivan/.bashrc ;ln -s /home/ivan/projectos/dotfiles/.bashrc  /home/ivan/.bashrc
+rm -fr /home/ivan/.zshrc ;ln -s /home/ivan/projectos/dotfiles/.zshrc  /home/ivan/.zshrc
 rm -fr /home/ivan/.gitconfig ;ln -s /home/ivan/projectos/dotfiles/.gitconfig  /home/ivan/.gitconfig
 rm -fr /home/ivan/.vimrc ;ln -s /home/ivan/projectos/dotfiles/.vimrc  /home/ivan/.vimrc
 rm -fr /home/ivan/.vim ;ln -s /home/ivan/projectos/dotfiles/.vim  /home/ivan/.vim
