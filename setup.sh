6f9b1c14b552ea71b9d2ba99713842d1ae540f94
Ivan Muller <mullerivan@gmail.com>
2015-06-09 18:39:10 -0300

    change position

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-rm -fr /home/ivan/.bashrc ;ln -s /home/ivan/projectos/dotfiles/.bashrc  /home/ivan/.bashrc
-rm -fr /home/ivan/.zshrc ;ln -s /home/ivan/projectos/dotfiles/.zshrc  /home/ivan/.zshrc
-rm -fr /home/ivan/.gitconfig ;ln -s /home/ivan/projectos/dotfiles/.gitconfig  /home/ivan/.gitconfig
-rm -fr /home/ivan/.vimrc ;ln -s /home/ivan/projectos/dotfiles/.vimrc  /home/ivan/.vimrc
-rm -fr /home/ivan/.vim ;ln -s /home/ivan/projectos/dotfiles/.vim  /home/ivan/.vim
-rm -fr /home/ivan/.hgrc ;ln -s /home/ivan/projectos/dotfiles/.hgrc  /home/ivan/.hgrc
-rm -fr  /home/ivan/.gemrc; ln -s /home/ivan/projectos/dotfiles/.gemrc  /home/ivan/.gemrc
-rm -fr  /etc/rc.local; ln -s /home/ivan/projectos/dotfiles/rc.local  /etc/rc.local
+rm -fr /home/ivan/.bashrc ;ln -s /home/ivan/.dotfiles/.bashrc  /home/ivan/.bashrc
+rm -fr /home/ivan/.zshrc ;ln -s /home/ivan/.dotfiles/.zshrc  /home/ivan/.zshrc
+rm -fr /home/ivan/.gitconfig ;ln -s /home/ivan/.dotfiles/.gitconfig  /home/ivan/.gitconfig
+rm -fr /home/ivan/.vimrc ;ln -s /home/ivan/.dotfiles/.vimrc  /home/ivan/.vimrc
+rm -fr /home/ivan/.vim ;ln -s /home/ivan/.dotfiles/.vim  /home/ivan/.vim
+rm -fr /home/ivan/.hgrc ;ln -s /home/ivan/.dotfiles/.hgrc  /home/ivan/.hgrc
+rm -fr  /home/ivan/.gemrc; ln -s /home/ivan/.dotfiles/.gemrc  /home/ivan/.gemrc
+rm -fr  /etc/rc.local; ln -s /home/ivan/.dotfiles/rc.local  /etc/rc.local
 #hosts files
-rm -fr /etc/hosts ;ln -s /home/ivan/projectos/dotfiles/hosts  /etc/hosts
+rm -fr /etc/hosts ;ln -s /home/ivan/.dotfiles/hosts  /etc/hosts
 
